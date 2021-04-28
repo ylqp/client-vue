b57e43114914d283b2591805c056eb0b70f50840
lanzhiyuan <you@example.com>
2021-04-28 18:14:36 +0800

    请求增加loading

--- src/http/request.js
+++ src/http/request.js
@@ -3,19 +3,27 @@
  * data: 参数
  * callback: 回调函数
  */
-
+import Vue from 'vue'
 const SENT = process.env.NODE_ENV === 'development' ? 'Sent_Post' : 'Sent'
-
+let loadingCount = 0
 const request = async (code, data) => {
+    const { $eventBus } = Vue.prototype
+    if (loadingCount === 0) {
+      $eventBus.$loading()
+    }
+    loadingCount++
     let cdata = null
     if (data) {
       cdata = JSON.stringify(data)
     }
     return new Promise((resolve, reject) => {
         window.OTS[SENT](code, null, cdata, function (rdata) {
-            
             let res = JSON.parse(rdata)
             let { status } = res
+            loadingCount--
+            if (loadingCount === 0) {
+              $eventBus.$loading().close()
+            }
             if (status === 1) {
               resolve(res)
             } else {
